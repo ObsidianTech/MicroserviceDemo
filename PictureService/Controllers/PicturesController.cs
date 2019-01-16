66e740dc96c0751ee9c85d6f501776c1ee1ab777
Christopher Smith <christopherjamessmithus@gmail.com>
2019-01-16 10:43:39 -0600

    Removing some boilerplate code and getting the controller ready for the DbContext.

--- PictureService/Controllers/PicturesController.cs
+++ PictureService/Controllers/PicturesController.cs
@@ -9,24 +9,32 @@ namespace PictureService.Controllers
     [Route("api/[controller]")]
     public class PicturesController : Controller
     {
+        private readonly ApplicationDbContext _context {get;set;}
+        
+        public PictureController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+        
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            
         }
 
         // PUT api/values/5
