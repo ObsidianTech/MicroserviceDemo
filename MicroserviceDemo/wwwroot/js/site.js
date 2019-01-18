4b44f4416a6da9a3418b24d36f5a6db6f4dded4b
Christopher Smith <csmith4work@gmail.com>
2019-01-18 07:16:11 -0600

    Designing the front end.

--- MicroserviceDemo/wwwroot/js/site.js
+++ MicroserviceDemo/wwwroot/js/site.js
@@ -1,20 +1,37 @@
-﻿$(document).ready(function (){
-var Picture = $("#Picture");
-var PictureName = $("#PictureName");
+﻿$(document).ready(function () {
 
-$.ajax({
-    url: 'http://18.217.145.205:81/api/Pictures',
-    type: "GET",
-    dataType: "json",
-    processData: true,
-    data: {},
-    success: function (response) {
-        console.log(success);
-        loadPicture(response);
-    }
-});
+    var Picture = $("#Picture");
 
-function loadPicture(data) {
-    Picture.attr('src', `data:image/jpg;base64,data.PictureFile`);
-};
+    var PictureName = $("#PictureName");
+
+
+
+    $.ajax({
+
+        url: 'http://18.217.145.205:81/api/Pictures/2',
+
+        type: "GET",
+
+        dataType: "json",
+
+        processData: true,
+
+        success: function (response) {
+
+            console.log("success");
+
+            loadPicture(response);
+
+        }
+
+    });
+
+
+
+    function loadPicture(data) {
+
+        Picture.attr('src', `data:image/jpg;base64,${data.pictureFile}`);
+        Picture.addClass("picture");
+    };
+
 });
